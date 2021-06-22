0a21f66320c3efc3725da846eedc5e51f8e054e1
Md. Samiur Rahman Mukul <sr.mukul9090@gmail.com>
2021-06-22 09:04:01 +0600

    code refactoring, create background workers & run workers loop

--- index.js
+++ index.js
@@ -1,39 +1,26 @@
 /**
- * Title: Uptime Monitoring RESTFull API Application
- * Description: A RESTFull API to monitor up or down time of user defined links
- * Author: Md. Samiur Rahman (Mukul)
- * Date: 1 July 2021 & 17 July 2021 (remodify)
+ * Title: Project Initial file
+ * Description: Initial file to start the node server and workers
+ * Author: Md. Samiur Rahman Mukul
+ * Date: 22 July 2021
  *
  */
 
 // * dependencies
-const http = require("http");
-const { handleReqRes } = require("./helpers/handleReqRes");
-
-/* // ? twilio sms api using send message
-const { sendTwilioSms } = require("./helpers/notifications");
-sendTwilioSms("01611111111", "Hello World", (err) => {
-  console.log(`this is the error: `, err);
-}); */
+const server = require("./lib/server");
+const workers = require("./lib/worker");
 
 // * app object - module scaffolding
 const app = {};
 
-// * configuration
-app.config = {
-  port: 3000,
+app.init = () => {
+  // start the server
+  server.init();
+  // start the workers
+  workers.init();
 };
 
-// * create server
-app.createServer = () => {
-  const server = http.createServer(app.handleReqRes);
-  server.listen(app.config.port, () => {
-    console.log(`server listening to port ${app.config.port}...`);
-  });
-};
+app.init();
 
-// * handle Request Response
-app.handleReqRes = handleReqRes;
-
-// * start the server
-app.createServer();
+// * export the app
+module.exports = app;
